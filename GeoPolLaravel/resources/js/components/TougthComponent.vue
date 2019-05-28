3b123c62b77ebcdfb78c7f5ea533ba9a6979514d
RoberthXLZ <roberthloor1@gmail.com>
2019-05-28 10:47:27 -0500

    small fix

---
+++ GeoPolLaravel/resources/js/components/TougthComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        hola
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- geopol/src/store/actions.js
+++ geopol/src/store/actions.js
@@ -1,4 +1,5 @@
 // import Vue from 'vue'
+import axios from 'axios'
 
 export default {
   /*
@@ -21,4 +22,38 @@ export default {
         })
     })
   } */
+
+  getBeneficiariosAll({commit}, Id){
+    /*return new Promise((resolve, reject) => {
+      Vue.http.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then((resp) => {
+          console.log('Entre:', resp.body.estado)
+          if (resp.body.estado) {
+            console.log(resp.body.estado)
+            commit('setProyectosPorZonas', resp.body.datos)
+            return resolve()
+          } else {
+            commit('setError', resp.body.datos)
+            return reject(resp.body.datos)
+          }
+        }).catch((err) => {
+          commit('setError', err)
+          return reject(err)
+        })
+    })*/
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"
+    const url = 'http://127.0.0.1:8000/beneficiario'
+    axios.get(url)
+      /*.then(function (response) {
+      console.log(response.data);
+      })*/
+      .then(response => response)
+      .then(contents => console.log(contents))
+      .catch(function (error) {
+      console.log('supererror',error);
+      });
+  }
+
+
+
 }
